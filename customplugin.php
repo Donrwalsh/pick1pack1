836462f52f4fb46203e82844d51e572c76c4dff2
Bigbrass <donrwalsh@gmail.com>
2017-02-25 14:30:48 -0600

    Added fix to wonky numbers in popular picks
    Does not count a pick if the last pick from that IP address was the
    selected card. (Still records in wp_inputs)

--- customplugin.php
+++ customplugin.php
@@ -9,7 +9,6 @@
 	License: GPL2
 	*/
 	
-add_action( 'init', 'my_script_enqueuer' );
 add_action("wp_ajax_my_card_pick", "my_card_pick");
 add_action("wp_ajax_nopriv_my_card_pick", "my_card_pick");
 
@@ -25,7 +24,14 @@ function my_card_pick() {
 	
 	global $wpdb;
 
+	$last_choice = $wpdb->get_results('SELECT card_Choice FROM `wp_inputs` WHERE user_IP = "' . $_SERVER['REMOTE_ADDR'] . '" ORDER BY ID DESC LIMIT 1');
+        $last_choice = array_shift($last_choice);
+
+	if((string)$last_choice->card_Choice != (string)$_REQUEST['choice']){
+
 	$wpdb->query('UPDATE wp_master SET Picks = Picks + 1 WHERE ID = ' . $_REQUEST['choice']);
+	
+	}
 	
 	global $wpdb;
 	
@@ -56,17 +62,5 @@ function my_card_pick() {
 
 }
 
-
-
-
-
-function my_script_enqueuer() {
-   wp_register_script( "my_voter_script", WP_PLUGIN_URL.'/customplugin/my_voter_script.js', array('jquery') );
-   wp_localize_script( 'my_voter_script', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));        
-
-   wp_enqueue_script( 'jquery' );
-   wp_enqueue_script( 'my_voter_script' );
-
-}
 
 ?>
